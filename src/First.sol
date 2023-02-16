902666b14ab976470736dac544af2e9e7d356c33
Kayacan Yıldız <kayazico@zico.modem.local>
2023-02-16 20:23:11 +0300

    test1

--- src/First.sol
+++ src/First.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-contract Exit {
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
+contract Exit is ERC721 {
+    constructor()ERC721("asd","ASD"){
+        
+    }
+    uint constant ab = 5;
+    function getProd() public view returns(uint){
+        return ab;
+    }
 }
